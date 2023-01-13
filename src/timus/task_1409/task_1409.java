8ab42885f3f248903c01b673c442cf27848560aa
anndy <122534663+anndy-mana@users.noreply.github.com>
2023-01-13 17:49:10 +0500

    Add files via upload

---
+++ src/timus/task_1409/task_1409.java
@@ -0,0 +1,22 @@
+package timus.task_1409;
+
+import java.util.Scanner;
+
+public class task_1409 {
+    public static void main(String[] args) {
+
+
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = a + b - 1;
+
+        System.out.println(c-a);
+        System.out.println(c-b);
+
+        in.close();
+
+
+    }
+}
